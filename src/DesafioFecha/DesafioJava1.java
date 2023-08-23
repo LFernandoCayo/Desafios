b23f1d06d56c5c613762cbc381627824443272f6
Fernando Cayo <luisfernandocayo@gmail.com>
2023-08-23 19:18:16 -0300

    Tareas desafio1

--- src/DesafioFecha/DesafioJava1.java
+++ src/DesafioFecha/DesafioJava1.java
@@ -1,10 +1,36 @@
 package DesafioFecha;
 
+import java.util.Scanner;
 public class DesafioJava1 {
-
+    
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+        Scanner scanner = new Scanner(System.in);
+	
+		System.out.println("Ingrese el dia:  ");
+		int dia = scanner.nextByte();
+		
+		System.out.println("Ingrese el mes:  ");
+		int mes = scanner.nextByte();
+		
+		System.out.println("Ingrese el año:  ");
+		int anio = scanner.nextByte();
+		
+		
+		if (dia >= 1 && dia <= 31 && mes >= 1 && mes <= 12 && anio >= 1900 && anio <= 2099) {
+		    // Verificar si febrero tiene 29 días en caso de ser año bisiesto
+		    boolean esBisiesto = (anio % 4 == 0 && anio % 100 != 0) || (anio % 400 == 0);
+		    if (mes == 2 && dia > 29 && !esBisiesto) {
+		        System.out.println("La fecha ingresada es incorrecta.");
+		    } else {
+		        System.out.println("La fecha ingresada es correcta.");
+		    }
+		} else {
+		    System.out.println("La fecha ingresada es incorrecta.");
+		
 
+	scanner.close();
+			
+		}
 	}
 
 }
